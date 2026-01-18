b893dda2210ebef7422515839bc32cadbe7b08b1
Azam Besharat <besharat@vt.edu>
2026-01-18 11:20:47 -0500

    my test with manual factorizing S by just adding S. I might use it later to have a separate function for factorize_S

--- tests/unit/test_spin.cpp
+++ tests/unit/test_spin.cpp
@@ -15,6 +15,7 @@
 #include <SeQuant/core/rational.hpp>
 #include <SeQuant/core/space.hpp>
 #include <SeQuant/core/tensor_canonicalizer.hpp>
+#include <SeQuant/core/utility/indices.hpp>
 #include <SeQuant/domain/mbpt/convention.hpp>
 #include <SeQuant/domain/mbpt/spin.hpp>
 
@@ -649,6 +650,171 @@ SECTION("partial spintracing + S_maps = full spintracing") {
 }
 
 SECTION("Symmetrize expression") {
+  // my test with manual factorizing S by just adding S:
+  {  // g * t1 + g * t1
+    auto input = ex<Tensor>(L"g", bra{L"a_1", L"a_2"}, ket{L"i_1", L"a_3"},
+                            Symmetry::Symm) *
+                     ex<Tensor>(L"t", bra{L"a_3"}, ket{L"i_2"}) +
+                 ex<Tensor>(L"g", bra{L"a_2", L"a_1"}, ket{L"i_2", L"a_3"},
+                            Symmetry::Symm) *
+                     ex<Tensor>(L"t", bra{L"a_3"}, ket{L"i_1"});
+
+    // auto result = factorize_S(input, {{L"i_1", L"a_1"}, {L"i_2", L"a_2"}});
+    // factorize_S(input, {{L"a_1", L"i_1"}, {L"a_2", L"i_2"}}); // with my fn
+
+    auto const ext_idxs = external_indices(input);
+
+    auto bixs = ext_idxs | ranges::views::transform(
+                               [](auto&& vec) { return get_bra_idx(vec); });
+    auto kixs = ext_idxs | ranges::views::transform(
+                               [](auto&& vec) { return get_ket_idx(vec); });
+    auto result =
+        ex<Tensor>(Tensor{L"S", bra(std::move(kixs)), ket(std::move(bixs))}) *
+        input;
+
+    std::wcout << "does it factorize S or not? " << to_latex_align(result)
+               << "\n";
+    std::wcout << "finish checking my test: "
+               << "\n";
+    // what will be the result?
+    // simplified fn with hleper gives correct restult for this
+    // or big fn with helper
+
+    REQUIRE_THAT(result,
+                 EquivalentTo("2 S{i1,i2;a1,a2} g{a1,a2;i2,a3}:S t{a3;i1}"));
+
+    auto input2 = ex<Tensor>(L"g", bra{L"a_1", L"a_2"}, ket{L"i_1", L"i_2"},
+                             Symmetry::Antisymm) +
+                  ex<Tensor>(L"g", bra{L"a_2", L"a_1"}, ket{L"i_1", L"i_2"},
+                             Symmetry::Antisymm);
+    // auto res = factorize_S(input2, {{L"i_1", L"a_1"}, {L"i_2", L"a_2"}});
+
+    // std::wcout << "let's see: ? " << to_latex_align(res) << "\n";
+
+    auto input3 = ex<Tensor>(L"g", bra{L"a_1", L"a_2"}, ket{L"i_1", L"i_2"},
+                             Symmetry::Antisymm) +
+                  ex<Tensor>(L"g", bra{L"a_1", L"a_2"}, ket{L"i_1", L"i_2"},
+                             Symmetry::Antisymm);
+
+    // this works fine with old factorize_S, I just turned on the canonicalizer
+    // auto res3 = factorize_S(input3, {{L"i_1", L"a_1"}, {L"i_2", L"a_2"}});
+
+    auto const ext_idxs3 = external_indices(input3);
+
+    auto bixs3 = ext_idxs3 | ranges::views::transform(
+                                 [](auto&& vec) { return get_bra_idx(vec); });
+    auto kixs3 = ext_idxs3 | ranges::views::transform(
+                                 [](auto&& vec) { return get_ket_idx(vec); });
+    auto res3 =
+        ex<Tensor>(Tensor{L"S", bra(std::move(kixs3)), ket(std::move(bixs3))}) *
+        input3;
+    simplify(res3);
+
+    std::wcout << "does it factorize S or not? " << to_latex_align(res3)
+               << "\n";
+    /*
+    & ({{{2}}{\bar{g}^{{i_1}{i_2}}_{{a_1}{a_2}}}})
+\end{align}
+does it factorize S or not? \begin{align}
+& {{{2}}{S^{{a_1}{a_2}}_{{i_1}{i_2}}}{\bar{g}^{{i_1}{i_2}}_{{a_1}{a_2}}}}
+\end{align}
+     */
+
+    auto input4 = ex<Tensor>(L"g", bra{L"a_1", L"a_2"}, ket{L"i_1", L"i_2"},
+                             Symmetry::Antisymm);
+    // auto res4 = factorize_S(input4, {{L"i_1", L"a_1"}, {L"i_2", L"a_2"}});
+    // std::wcout << "does it factorize S or not? " << to_latex_align(res4)
+    //            << "\n";
+    // after adding sum it works correctly:
+    // \begin{align}
+    // & {{S^{{a_1}{a_2}}_{{i_1}{i_2}}}{\bar{g}^{{i_1}{i_2}}_{{a_1}{a_2}}}}
+    // \end{align}
+  }
+
+  {  // g * t1 * t1 * t1 + g * t1 * t1 * t1
+    auto input = ex<Tensor>(L"g", bra{L"i_3", L"i_4"}, ket{L"i_1", L"a_3"},
+                            Symmetry::Symm) *
+                     ex<Tensor>(L"t", bra{L"a_1"}, ket{L"i_3"}) *
+                     ex<Tensor>(L"t", bra{L"a_2"}, ket{L"i_4"}) *
+                     ex<Tensor>(L"t", bra{L"a_3"}, ket{L"i_2"}) +
+                 ex<Tensor>(L"g", bra{L"i_3", L"i_4"}, ket{L"i_2", L"a_3"},
+                            Symmetry::Symm) *
+                     ex<Tensor>(L"t", bra{L"a_2"}, ket{L"i_3"}) *
+                     ex<Tensor>(L"t", bra{L"a_1"}, ket{L"i_4"}) *
+                     ex<Tensor>(L"t", bra{L"a_3"}, ket{L"i_1"});
+    // auto result = factorize_S(input, {{L"i_1", L"a_1"}, {L"i_2", L"a_2"}});
+
+    auto const ext_idxs = external_indices(input);
+
+    auto bixs = ext_idxs | ranges::views::transform(
+                               [](auto&& vec) { return get_bra_idx(vec); });
+    auto kixs = ext_idxs | ranges::views::transform(
+                               [](auto&& vec) { return get_ket_idx(vec); });
+    auto result =
+        ex<Tensor>(Tensor{L"S", bra(std::move(kixs)), ket(std::move(bixs))}) *
+        input;
+
+    REQUIRE_THAT(
+        result,
+        EquivalentTo(
+            "2S{i3,i4;a2,a1} g{i1,i2;i4,a3}:S t{a1;i1} t{a2;i2} t{a3;i3}"));
+    // std::wcout << "how it factorize it? " << to_latex_align(result) << "\n";
+
+    auto input2 = ex<Tensor>(L"g", bra{L"a_1", L"a_2"}, ket{L"i_1", L"a_3"},
+                             Symmetry::Symm) *
+                      ex<Tensor>(L"t", bra{L"a_3"}, ket{L"i_2"}) +
+                  ex<Tensor>(L"g", bra{L"a_2", L"a_1"}, ket{L"i_2", L"a_3"},
+                             Symmetry::Symm) *
+                      ex<Tensor>(L"t", bra{L"a_3"}, ket{L"i_1"}) +
+                  ex<Tensor>(L"g", bra{L"i_3", L"i_4"}, ket{L"i_1", L"a_3"},
+                             Symmetry::Symm) *
+                      ex<Tensor>(L"t", bra{L"a_1"}, ket{L"i_3"}) *
+                      ex<Tensor>(L"t", bra{L"a_2"}, ket{L"i_4"}) *
+                      ex<Tensor>(L"t", bra{L"a_3"}, ket{L"i_2"}) +
+                  ex<Tensor>(L"g", bra{L"i_3", L"i_4"}, ket{L"i_2", L"a_3"},
+                             Symmetry::Symm) *
+                      ex<Tensor>(L"t", bra{L"a_2"}, ket{L"i_3"}) *
+                      ex<Tensor>(L"t", bra{L"a_1"}, ket{L"i_4"}) *
+                      ex<Tensor>(L"t", bra{L"a_3"}, ket{L"i_1"});
+    // auto rest2 = factorize_S(input2, {{L"i_1", L"a_1"}, {L"i_2", L"a_2"}});
+    // std::wcout << "the output of input 2 which are two different terms: "
+    //            << to_latex_align(rest2) << "\n";
+  }
+
+  {  // 2 g * t1 * t1 * t2 + 2 g * t1 * t1 * t2
+    auto input =
+        ex<Constant>(2) *
+            ex<Tensor>(L"g", bra{L"i_3", L"i_4"}, ket{L"a_3", L"a_4"},
+                       Symmetry::Symm) *
+            ex<Tensor>(L"t", bra{L"a_3"}, ket{L"i_3"}) *
+            ex<Tensor>(L"t", bra{L"a_2"}, ket{L"i_4"}) *
+            ex<Tensor>(L"t", bra{L"a_1", L"a_4"}, ket{L"i_1", L"i_2"}) +
+        ex<Constant>(2) *
+            ex<Tensor>(L"g", bra{L"i_3", L"i_4"}, ket{L"a_3", L"a_4"},
+                       Symmetry::Symm) *
+            ex<Tensor>(L"t", bra{L"a_3"}, ket{L"i_3"}) *
+            ex<Tensor>(L"t", bra{L"a_1"}, ket{L"i_4"}) *
+            ex<Tensor>(L"t", bra{L"a_2", L"a_4"}, ket{L"i_2", L"i_1"});
+    // auto result = factorize_S(input, {{L"i_1", L"a_1"}, {L"i_2", L"a_2"}});
+
+    auto const ext_idxs = external_indices(input);
+
+    auto bixs = ext_idxs | ranges::views::transform(
+                               [](auto&& vec) { return get_bra_idx(vec); });
+    auto kixs = ext_idxs | ranges::views::transform(
+                               [](auto&& vec) { return get_ket_idx(vec); });
+    auto result =
+        ex<Tensor>(Tensor{L"S", bra(std::move(kixs)), ket(std::move(bixs))}) *
+        input;
+    simplify(result);
+    REQUIRE(result->is<Sum>() == false);
+    // REQUIRE_THAT(result, EquivalentTo("4 S{i1,i2;a1,a2} g{i3,i4;a3,a4} "
+    //                                   "t{a4;i3} t{a2;i4} t{a1,a3;i1,i2}"));
+
+    // with expansion:
+    // 4 S{i_1,i_2;a_1,a_2}:N-C-S * g{i_3,i_4;a_3,a_4}:S-C-S * t{a_1;i_3}:N-C-S
+    // * t{a_3;i_4}:N-C-S * t{a_2,a_4;i_1,i_2}:N-C-S
+  }
   {
     // g * t1 + g * t1
     auto input = ex<Tensor>(L"g", bra{L"a_1", L"a_2"}, ket{L"i_1", L"a_3"},
